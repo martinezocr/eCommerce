0af3d895f5edb2ea4826bde5477addcf16542ca9
martinezocr <oscar19@live.com.ar>
2021-08-27 18:13:08 -0300

    ajutes en carousel, generacion dinamica

--- Source/eCommerce.Web/eCommerce.Web/ClientApp/src/app/utils/carousel/carousel.component.ts
+++ Source/eCommerce.Web/eCommerce.Web/ClientApp/src/app/utils/carousel/carousel.component.ts
@@ -7,7 +7,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit, OnDestroy {
   interval;
-  slideIndex: number = 1;
+  slideIndex: number = 0;
+  images = [
+    'https://i.pinimg.com/originals/d8/80/4f/d8804f454d8041073ec2d883b4e70afb.jpg',
+    'https://i.pinimg.com/originals/54/d5/d3/54d5d31c30a900a2e9b18659c2b7d4f5.jpg',
+    'https://img.wattpad.com/06cfc8e7101b95e749749441372cd76cd349607f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f3831774436586e53766c765152673d3d2d3133352e313463386539366531636366646162393834373739363138313331322e676966'
+  ]
   constructor() { }
 
   ngOnDestroy(): void {
@@ -19,7 +24,7 @@ export class CarouselComponent implements OnInit, OnDestroy {
   }
 
   createInterval() {
-    this.interval = setInterval(() => this.plusSlides(1), 5000);
+    this.interval = setInterval(() => this.plusSlides(0), 5000);
   }
 
   plusSlides(n) {
@@ -36,10 +41,10 @@ export class CarouselComponent implements OnInit, OnDestroy {
   }
 
   showSlides(n) {
-    if (this.slideIndex > 3)
-      this.slideIndex = 1;
-    if (this.slideIndex < 1)
-      this.slideIndex = 3;
+    if (this.slideIndex > this.images.length - 1)
+      this.slideIndex = 0;
+    if (this.slideIndex < 0)
+      this.slideIndex = this.images.length - 1;
   }
 
 }
